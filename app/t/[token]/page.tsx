b2caad3b40fa987ecd94a661d9c84fec6580867b
manriquedario-eng <manriquedario@gmail.com>
2026-09-24 19:00:01 -0300

    Sync WhatsApp: app/t/[token]/page.tsx

--- app/t/[token]/page.tsx
+++ app/t/[token]/page.tsx
@@ -115,8 +115,8 @@ export default async function PublicAppointmentPage({
               <input type="hidden" name="token" value={token} />
               <button className="btn" type="submit">Confirmar turno</button>
             </form>
-            <Link className="btn secondary" href={`/t/${token}?action=reschedule`}>Solicitar reprogramación</Link>
             <Link className="btn-ghost danger" href={`/t/${token}?action=cancel`}>Cancelar turno</Link>
+            <Link className="btn secondary" href={`/t/${token}?action=reschedule`}>Solicitar reprogramación</Link>
           </div>
         )}
 
